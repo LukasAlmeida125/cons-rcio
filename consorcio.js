9c3a0735ecc433027948091a8688bbb9a0240fa2
Lukas <lucas.lauriano125@gmail.com>
2024-04-16 16:54:42 -0300

    site de consórcio

---
+++ consorcio.js
@@ -0,0 +1,14 @@
+// JavaScript para rolagem suave ao clicar nos links do menu
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop - 50,
+            behavior: 'smooth'
+        });
+    });
+});
